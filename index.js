15adfdce449152759edc43a0ecc8ed6669196cf7
Ginger Heef <gingerheef003@gmail.com>
2023-12-31 05:27:49 +0530

    echo simplified

--- index.js
+++ index.js
@@ -2,34 +2,19 @@ const http = require("http");
 
 http
   .createServer((request, response) => {
-    const { headers, method, url } = request;
-    let body = [];
-
-    request
-      .on("error", (err) => {
-        console.error(err);
-      })
-      .on("data", (chunk) => {
-        body.push(chunk);
-      })
-      .on("end", () => {
-        body = Buffer.concat(body).toString();
-
-        response.on("error", (err) => {
-          console.error(err);
+    if (request.method === "POST" && request.url === "/echo") {
+      let body = [];
+      request
+        .on("data", (chunk) => {
+          body.push(chunk);
+        })
+        .on("end", () => {
+          body = Buffer.concat(body).toString();
+          response.end(body);
         });
-
-        // response.statusCode = 200;
-        // response.setHeader("Content-Type", "application/json");
-        response.writeHead(200, { "Content-Type": "application/json" });
-
-        const responseBody = { headers, method, url, body };
-
-        // response.write(JSON.stringify(responseBody));
-        // response.end();
-        response.end(JSON.stringify(responseBody))
-      });
+    } else {
+      response.statusCode = 404;
+      response.end();
+    }
   })
-  .listen(8080, host, () => {
-    console.log(`Server running at http://${host}:${port}`);
-  });
+  .listen(8000);
