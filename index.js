d3c933391f8c73e6042ab2bae3a66bedfdf40532
Ginger Heef <gingerheef003@gmail.com>
2023-12-31 05:28:18 +0530

    echo even simplified

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const http = require("http");
 http
   .createServer((request, response) => {
     if (request.method === "POST" && request.url === "/echo") {
-      let body = [];
-      request
-        .on("data", (chunk) => {
-          body.push(chunk);
-        })
-        .on("end", () => {
-          body = Buffer.concat(body).toString();
-          response.end(body);
-        });
+      request.pipe(response)
     } else {
       response.statusCode = 404;
       response.end();
